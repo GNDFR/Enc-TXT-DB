4d7b93de1e152b611028a157bf83df615d57d337
GNDFR <kimhyowoo435@gmail.com>
2025-06-29 15:28:25 +0900

    Delete backend directory

--- backend/lib/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-let conn = null;
-
-export default async function dbConnect() {
-  if (conn) return;
-  conn = await mongoose.connect(process.env.MONGODB_URI);
-}
--- backend/lib/note.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const NoteSchema = new mongoose.Schema({
-  identifier: { type: String, unique: true },
-  encryptedData: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.models.Note || mongoose.model('Note', NoteSchema);
